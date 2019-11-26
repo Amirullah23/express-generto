e72f87556b783932cd17f57c8ce0f687dc0fcae9
amirullah23 <amirullah.kasang@gmail.com>
2019-11-26 14:59:03 +0700

    add usera

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('from user .js');
+});
+router.get('/amir', function(req, res, next) {
+  res.send('user and amir');
 });
 
 module.exports = router;
